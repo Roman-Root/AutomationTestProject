8c9871cc41aeecef1e4c3678d60655ae96c0fdf8
roman44root <korneev.roman.viktorovich@gmail.com>
2022-12-11 22:23:21 +0500

    Page Objeckt Start

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    private By singUp = By.xpath("//*[@id='root']/div/div/div[4]/div[1]/div/div[2]/form/div[1]/div[1]/span/span");
+    private By emailField = By.xpath("//*[@id='modalusername']");
+    private By showPassword = By.xpath("//*[@id='root']/div/div/div[4]/div[1]/div/div[2]/form/div[2]/div[1]/span");
+    private By passwordField = By.xpath("//*[@id='current-password']");
+    private By loginButton = By.xpath("//*[@id='root']/div/div/div[4]/div[1]/div/div[4]/div[1]/button");
+    private By forgotPassword = By.xpath("//*[@id='root']/div/div/div[4]/div[1]/div/div[4]/div[2]/a");
+
+    public LoginPage typeEmail(String email){
+        driver.findElement(emailField).sendKeys(email);
+        return this;
+    }
+    public LoginPage typePassword(String password){
+        driver.findElement(passwordField).sendKeys(password);
+        return this;
+    }
+    public LoginPage clickLoginButton(){
+        driver.findElement(loginButton).click();
+        return this;
+    }
+    public LoginPage registerAccount(String email, String password){
+        this.typeEmail(email);
+        this.typePassword(password);
+        this.clickLoginButton();
+        return new LoginPage(driver);
+    }
+}
---
+++ src/main/java/StartPage.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class StartPage {
+    private WebDriver driver;
+
+    public StartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    private By upgrade = By.xpath("//div[3]/div[2]/a[1]");
+    private By getCertified = By.xpath("//*[@id='cert_navbtn']");
+    private By free_website = By.xpath("//div[3]/div[2]/a[1]");
+    private By login = By.xpath("//*[@id='w3loginbtn']");
+    private By field = By.xpath("//*[@id='search2']");
+    private By thref = By.xpath("//*[@id='main']/div[1]/div/h4/a");
+
+    public LoginPage clickLogin(){
+        driver.findElement(login).click();
+        return new LoginPage(driver);
+    }
+
+}
