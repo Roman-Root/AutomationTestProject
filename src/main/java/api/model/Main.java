cab740a6f20ebc6b5040cc26fae8519c5d8c6312
Roman Korneev <korneev.roman.viktorovich@gmail.com>
2023-07-23 11:13:01 +0500

    Added new method for connect to Electron JS application

---
+++ src/main/java/api/model/Main.java
@@ -0,0 +1,25 @@
+package api.model;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+//        ChromeOptions options = new ChromeOptions();
+//        options.setHeadless(false);
+//        WebDriver driver = new ChromeDriver(options);
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Connect to Electron JS application
+
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+//        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//        options.setBinary("/home/roman/Documents/folder-project/front/folder_project");
+//        options.addArguments("--port=9515");
+//        WebDriver driver = new ChromeDriver(options);
+//        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+//        driver.get("http://localhost:8000");
+    }
+}
--- src/main/java/api/model/specification/Specification.java
+++ src/main/java/api/model/specification/Specification.java
@@ -3,6 +3,7 @@ package api.model.specification;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.internal.ResponseSpecificationImpl;
 import io.restassured.specification.RequestSpecification;
@@ -20,18 +21,21 @@ public class Specification {
     public static ResponseSpecification responseSpecification200(){
         return new ResponseSpecBuilder()
                 .expectStatusCode(200)
+                .log(LogDetail.ALL)
                 .build();
     }
 
     public static ResponseSpecification responseSpecification400(){
         return new ResponseSpecBuilder()
-                .expectStatusCode(400)
+                .expectStatusCode(200)
+                .log(LogDetail.ALL)
                 .build();
     }
 
     public static ResponseSpecification responseSpecificationUnique(int status){
         return new ResponseSpecBuilder()
-                .expectStatusCode(status)
+                .expectStatusCode(200)
+                .log(LogDetail.ALL)
                 .build();
     }
 
